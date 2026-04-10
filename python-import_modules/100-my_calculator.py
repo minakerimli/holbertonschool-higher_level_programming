ad39e7862056a573cfa739bf982c8ea8930873fe
minakerimli <minaakerimli@gmail.com>
2026-04-10 21:26:07 +0000

    .

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,16 +3,17 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    argc = len(sys.argv)
-
-    if argc != 4:
+    # Check the number of arguments (Script + 3 args = 4)
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    # Assign and cast variables
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
 
+    # Handle operations based on the operator string
     if op == "+":
         result = add(a, b)
     elif op == "-":
@@ -22,7 +23,9 @@ if __name__ == "__main__":
     elif op == "/":
         result = div(a, b)
     else:
+        # Error message if the operator is not recognized
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
+    # Print the result in the format: <a> <operator> <b> = <result>
     print("{} {} {} = {}".format(a, op, b, result))
