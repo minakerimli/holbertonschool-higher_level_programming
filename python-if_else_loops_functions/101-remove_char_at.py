48e57a3d1b132eb1667e768f9a7ca8838dd8d192
minakerimli <minaakerimli@gmail.com>
2026-04-10 15:38:10 -0400

    .

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0:
+        return str
+
+    result = ""
+    for i in range(len(str)):
+        if i != n:
+            result += str[i]
+    return result
