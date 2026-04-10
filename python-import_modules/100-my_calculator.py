c975c0d5e6013484dd27a3ffedbabaae9c3a4cd9
minakerimli <minaakerimli@gmail.com>
2026-04-10 21:30:37 +0000

    .

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,17 +3,17 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    # Check if the number of arguments is exactly 3 (total 4 with script name)
+    # Check if the number of arguments is exactly 3 (4 including script name)
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Casting arguments and identifying operator
+    # Cast variables to integers
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Comparison logic for operators
+    # Check operator and perform calculation
     if op == "+":
         result = add(a, b)
     elif op == "-":
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     elif op == "/":
         result = div(a, b)
     else:
-        # Exact error message required by the task
+        # Note the specific error message required
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Formatted output
+    # Print the formatted result
     print("{} {} {} = {}".format(a, op, b, result))
