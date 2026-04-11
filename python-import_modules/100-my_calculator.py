66b1232cf4ada835a2dd263f33e461ead99a53d9
minakerimli <minaakerimli@gmail.com>
2026-04-11 16:58:50 +0000

    .

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,30 +2,37 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    # Check if the number of arguments is exactly 3 (4 including script name)
+
+def main():
+    """
+    Handles basic arithmetic operations based on command-line arguments.
+    Usage: ./100-my_calculator.py <a> <operator> <b>
+    """
+    # Check the number of arguments (including the script name, we expect 4)
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Cast variables to integers
     a = int(sys.argv[1])
-    op = sys.argv[2]
+    operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Check operator and perform calculation
-    if op == "+":
-        result = add(a, b)
-    elif op == "-":
-        result = sub(a, b)
-    elif op == "*":
-        result = mul(a, b)
-    elif op == "/":
-        result = div(a, b)
-    else:
-        # Note the specific error message required
+    # Map operators to their respective functions
+    ops = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    # Validate the operator and perform the calculation
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Print the formatted result
-    print("{} {} {} = {}".format(a, op, b, result))
+    result = ops[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    main()
