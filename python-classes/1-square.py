df9dc2c80889dcaec59400509a65f244d124a1dc
minakerimli <minaakerimli@gmail.com>
2026-04-11 15:53:36 +0000

    .

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size."""
+        self.__size = size
