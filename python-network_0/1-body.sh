7ec47f6082b74399e8064d7764644c451bfe3a6e
minakerimli <minaakerimli@gmail.com>
2026-04-18 16:37:30 +0000

    .

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends GET request and displays body only if response code is 200
-resp=$(curl -s -w "%{http_code}" "$1"); code=${resp: -3}; [ "$code" -eq 200 ] && echo "${resp%???}"
+# Sends GET request and displays response body
+printf "%s" "$(curl -s "$1")"
