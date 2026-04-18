3bbc66b3e257252191c3e8aff1ae7cbbd999f091
minakerimli <minaakerimli@gmail.com>
2026-04-18 16:14:44 +0000

    .

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Script that shows the full description of the table first_table
-SELECT CREATE TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_NAME = 'first_table'
-AND TABLE_SCHEMA = DATABASE();
+SHOW CREATE TABLE first_table;
