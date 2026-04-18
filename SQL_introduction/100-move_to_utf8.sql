90fe8371c358046753b9b672b7704b18fdad611e
minakerimli <minaakerimli@gmail.com>
2026-04-18 18:03:24 +0000

    .

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Convert hbtn_0c_0 database and its content to UTF8 (utf8mb4)
+-- Convert database hbtn_0c_0 to UTF8 (utf8mb4)
 
 -- Convert database charset and collation
 ALTER DATABASE hbtn_0c_0
@@ -10,8 +10,7 @@ ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert column name charset and collation
+-- Convert only column collation (no CHARACTER SET here)
 ALTER TABLE hbtn_0c_0.first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
