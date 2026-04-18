c374e82dce8647dea96c01903ef95dd6d9e09abd
minakerimli <minaakerimli@gmail.com>
2026-04-18 17:00:05 +0000

    .

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and handles HTTP errors by printing the status code."""
+
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
