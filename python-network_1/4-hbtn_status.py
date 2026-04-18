cfc8f5a0c7974f4ff39979c8c6ecfb05192d5ead
minakerimli <minaakerimli@gmail.com>
2026-04-18 17:04:21 +0000

    .

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetch a URL using requests and display response body info."""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    headers = {"cfclearance": "true"}
+
+    response = requests.get(url, headers=headers)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
